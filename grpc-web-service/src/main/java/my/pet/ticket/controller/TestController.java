72258c4c2a9f1bcfc7502fa9055d99a21085b12f
Pryanichnikov Sergey <s.pryanichnikov@ilnsk.bizml.ru>
2024-04-08 13:25:51 +0700

    refactoring pom

--- grpc-web-service/src/main/java/my/pet/ticket/controller/TestController.java
+++ grpc-web-service/src/main/java/my/pet/ticket/controller/TestController.java
@@ -1,22 +1,23 @@
 package my.pet.ticket.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+//import lombok.AllArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
 import my.pet.ticket.config.dto.AppProperties;
 import my.pet.ticket.entity.Ticket;
 import my.pet.ticket.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
 
 @RestController("/test")
-@AllArgsConstructor
-@Slf4j
 public class TestController {
 
+    @Autowired
     AppProperties appProperties;
 
+    @Autowired
     TicketRepository ticketRepository;
 
     @GetMapping("/user")
@@ -24,8 +25,8 @@ public class TestController {
         Ticket ticket = new Ticket();
         ticket.setName("1111111");
         ticketRepository.save(ticket);
-
-        log.info("{}", ticketRepository.findAll());
-        log.info("{}", appProperties.getTestUrl());
+//
+//        log.info("{}", ticketRepository.findAll());
+//        log.info("{}", appProperties.getTestUrl());
     }
 }
