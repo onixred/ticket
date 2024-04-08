c3acebf34d04e937438b19dd7927b47ff45edd82
Pryanichnikov Sergey <s.pryanichnikov@ilnsk.bizml.ru>
2024-04-08 10:23:40 +0700

    refactoring

--- src/main/java/my/pet/ticket/controller/TestController.java
+++ src/main/java/my/pet/ticket/controller/TestController.java
@@ -1,34 +1,31 @@
 package my.pet.ticket.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import my.pet.ticket.config.dto.AppProperties;
 import my.pet.ticket.entity.Ticket;
 import my.pet.ticket.repository.TicketRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 @RestController("/test")
+@AllArgsConstructor
+@Slf4j
 public class TestController {
 
-    @Autowired
     AppProperties appProperties;
 
-    @Autowired
     TicketRepository ticketRepository;
 
     @GetMapping("/user")
     public void test() throws SQLException {
-
         Ticket ticket = new Ticket();
         ticket.setName("1111111");
         ticketRepository.save(ticket);
 
-        System.out.println(ticketRepository.findAll());
-        System.out.println(appProperties.getTestUrl());
+        log.info("{}", ticketRepository.findAll());
+        log.info("{}", appProperties.getTestUrl());
     }
 }
--- src/main/java/my/pet/ticket/sevice/TicketService.java
+++ src/main/java/my/pet/ticket/sevice/TicketService.java
@@ -27,18 +27,16 @@ public class TicketService extends TicketServiceGrpc.TicketServiceImplBase {
     @Override
     public void getBalanceByTicketId(TicketRequest request, StreamObserver<ResponseTicket> responseObserver) {
         super.getBalanceByTicketId(request, responseObserver);
-        List<Long> requestIds = request.getTicketIdList();
-        List<Ticket> list = ticketRepository.findAllByIdIn(requestIds);
-
-        List<TicketDto> tickets = list.stream().map(ticket -> modelMapper.map(ticket, TicketDto.class)).toList();
+        List<Ticket> list = ticketRepository.findAllByIdIn(request.getTicketIdList());
 
         if (list.isEmpty()) {
             Metadata.Key<ErrorMessageGrpc> messageGrpcKey = ProtoUtils.keyForProto(ErrorMessageGrpc.getDefaultInstance());
-            ErrorMessageGrpc response = ErrorMessageGrpc.newBuilder().setCode(404).setMessage("не найдено").build();
+            ErrorMessageGrpc response = ErrorMessageGrpc.newBuilder().setCode(404).setMessage("не найдено тикеты по id").build();
             Metadata metadata = new Metadata();
             metadata.put(messageGrpcKey, response);
             responseObserver.onError(NOT_FOUND.asException(metadata));
         } else {
+            List<TicketDto> tickets = list.stream().map(ticket -> modelMapper.map(ticket, TicketDto.class)).toList();
             ResponseTicket responseTicket = ResponseTicket.newBuilder().addAllList(tickets).build();
             responseObserver.onNext(responseTicket);
             responseObserver.onCompleted();
