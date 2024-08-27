e390aff2c60498e417365cadc9c7cdaa9278447f
Alexei Baranov <baranov.alexalex@gmail.com>
2024-08-27 09:47:32 +0700

    #10-внес правки в соответствии с замечаниями

--- src/main/java/my/pet/ticket/etcd/EtcdConfiguration.java
+++ src/main/java/my/pet/ticket/etcd/EtcdConfiguration.java
@@ -1,7 +1,6 @@
 package my.pet.ticket.etcd;
 
 import io.etcd.jetcd.Client;
-import java.util.concurrent.ExecutionException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
