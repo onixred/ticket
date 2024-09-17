c3f04fbdfe16b4bea9c8f859eb48983dbc75383b
ilss8811 <main.ilss8811@gmail.com>
2024-09-17 11:16:24 +0700

    Additional logging tests

--- ticket-server/src/test/java/my/pet/ticket/server/logging/LogTest.java
+++ ticket-server/src/test/java/my/pet/ticket/server/logging/LogTest.java
@@ -1,24 +1,65 @@
 package my.pet.ticket.server.logging;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class LogTest {
 
     @Test
     void testMessageDetail() {
-        Log.WARN("Test message", new DefaultDetail("100", "Description"));
+        String expected = "Message: Test message | Details: [{\"errorCode\":\"100\",\"description\":\"Description\"}]";
+        String actual = Log.writeLogToString(
+                "Test message",
+                new DefaultDetail("100", "Description")
+        );
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testMessageDetailNoDescription() {
+        String expected = "Message: Test message | Details: [{\"errorCode\":\"100\",\"description\":null}]";
+        String actual = Log.writeLogToString(
+                "Test message",
+                new DefaultDetail("100")
+        );
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void testMessageEventDetail() {
-        Log.WARN("Test message", Events.BP_START.getEvent(), new DefaultDetail("100", "Description"));
+        String expected = "Message: Test message | Event: {\"name\":\"Business process start\"} | Details: [{\"errorCode\":\"100\",\"description\":\"Description\"}]";
+        String actual = Log.writeLogToString(
+                "Test message",
+                Events.BP_START.getEvent(),
+                new DefaultDetail("100", "Description")
+        );
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void testMessageEventCaseDetail() {
-        Log.WARN("Test message", Events.BP_START.getEvent(), new NullPointerException("Test cause message"), new DefaultDetail("100", "Description"));
+        String expected = "Message: Test message | Event: {\"name\":\"Business process start\"} | Cause: Test cause message | Details: [{\"errorCode\":\"100\",\"description\":\"Description\"}]";
+        String actual = Log.writeLogToString(
+                "Test message",
+                Events.BP_START.getEvent(),
+                new NullPointerException("Test cause message"),
+                new DefaultDetail("100", "Description")
+        );
+        Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void testAllNull() {
+        String expected = "Message: null | Event: null | Cause: null | Details: null";
+        String actual = Log.writeLogToString(
+                null,
+                null,
+                null,
+                null
+        );
+        Assertions.assertEquals(expected, actual);
+    }
+
 
 }
