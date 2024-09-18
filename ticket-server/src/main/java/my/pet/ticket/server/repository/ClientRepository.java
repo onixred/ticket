12c95e7f24ec3f5ba60b5a5d52a5e4bcc6769318
ilss8811 <main.ilss8811@gmail.com>
2024-09-18 15:07:43 +0700

    Delete unrelated files

--- ticket-server/src/main/java/my/pet/ticket/server/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package my.pet.ticket.server.repository;
-
-import my.pet.ticket.server.dao.ClientDao;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends CrudRepository<ClientDao, Long> {
-}
