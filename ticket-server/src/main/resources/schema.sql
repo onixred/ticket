62815cac86569e1bcfe1e77fb595ae5716466759
ilss8811 <main.ilss8811@gmail.com>
2024-09-19 12:18:11 +0700

    Varchar limit

--- ticket-server/src/main/resources/schema.sql
+++ ticket-server/src/main/resources/schema.sql
@@ -33,11 +33,11 @@ CREATE SEQUENCE pet_project.client_id_pk_seq START 1001;
 
 CREATE TABLE pet_project.clients (
     client_id BIGINT PRIMARY KEY DEFAULT nextval('pet_project.client_id_pk_seq'),
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    sur_name VARCHAR NOT NULL,
-    full_name VARCHAR NOT NULL,
-    email VARCHAR UNIQUE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    sur_name VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     deleted BOOLEAN NOT NULL DEFAULT false
