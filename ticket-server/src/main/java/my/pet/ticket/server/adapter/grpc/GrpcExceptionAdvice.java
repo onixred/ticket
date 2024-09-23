2d44c46afc5ce7edac0ee24cb6c31c86bde6c04d
ilss8811 <main.ilss8811@gmail.com>
2024-09-23 14:34:27 +0700

    Additional exception handlers

--- ticket-server/src/main/java/my/pet/ticket/server/adapter/grpc/GrpcExceptionAdvice.java
+++ ticket-server/src/main/java/my/pet/ticket/server/adapter/grpc/GrpcExceptionAdvice.java
@@ -2,6 +2,8 @@ package my.pet.ticket.server.adapter.grpc;
 
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
+import my.pet.ticket.server.adapter.persistence.PersistenceAdapterException;
+import my.pet.ticket.server.application.domain.service.DomainServiceException;
 import net.devh.boot.grpc.server.advice.GrpcAdvice;
 import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -12,6 +14,18 @@ public class GrpcExceptionAdvice {
   @GrpcExceptionHandler(DataIntegrityViolationException.class)
   public StatusRuntimeException dataIntegrityViolationException(
       DataIntegrityViolationException exception) {
+    return Status.INVALID_ARGUMENT.withDescription(exception.getMessage()).asRuntimeException();
+  }
+
+  @GrpcExceptionHandler(DomainServiceException.class)
+  public StatusRuntimeException domainServiceException(
+      DomainServiceException exception) {
+    return Status.INVALID_ARGUMENT.withDescription(exception.getMessage()).asRuntimeException();
+  }
+
+  @GrpcExceptionHandler(PersistenceAdapterException.class)
+  public StatusRuntimeException persistenceAdapterException(
+      PersistenceAdapterException exception) {
     return Status.INVALID_ARGUMENT.withDescription(exception.getMessage()).asRuntimeException();
   }
 
