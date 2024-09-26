3d99987ecdee45af854c176393e6761f43ac5f78
ilss8811 <main.ilss8811@gmail.com>
2024-09-26 14:05:31 +0700

    Fix layers

--- ticket-server/src/main/java/my/pet/ticket/server/common/security/AuthenticationManagerImpl.java
+++ ticket-server/src/main/java/my/pet/ticket/server/common/security/AuthenticationManagerImpl.java
@@ -1,6 +1,12 @@
 package my.pet.ticket.server.common.security;
 
+import java.util.List;
+import my.pet.ticket.server.application.domain.model.Token;
+import my.pet.ticket.server.application.domain.model.User;
+import my.pet.ticket.server.application.domain.service.TokenService;
+import my.pet.ticket.server.application.domain.service.UserService;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
@@ -8,9 +14,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuthenticationManagerImpl implements AuthenticationManager {
 
+  private final TokenService tokenService;
+
+  private final UserService userService;
+
+  public AuthenticationManagerImpl(TokenService tokenService, UserService userService) {
+    this.tokenService = tokenService;
+    this.userService = userService;
+  }
+
   @Override
   public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-    return authentication;
+    if (authentication != null && authentication.getPrincipal() != null) {
+      Token token = this.tokenService.getTokenByToken((String) authentication.getPrincipal());
+      User user = this.userService.get(token.getUserId());
+      BearerAuthenticationToken bearerAuthenticationToken = new BearerAuthenticationToken(
+          token.getUserId(), token.getToken(), List.of(new GrantedAuthorityImpl(user.getRole())));
+      bearerAuthenticationToken.setAuthenticated(true);
+      return bearerAuthenticationToken;
+    }
+    throw new BadCredentialsException("1");
   }
 
 }
--- ticket-server/src/main/java/my/pet/ticket/server/common/security/BearerAuthenticationReader.java
+++ ticket-server/src/main/java/my/pet/ticket/server/common/security/BearerAuthenticationReader.java
@@ -5,10 +5,6 @@ import io.grpc.Metadata.Key;
 import io.grpc.ServerCall;
 import java.util.List;
 import javax.annotation.Nullable;
-import my.pet.ticket.server.application.domain.model.Token;
-import my.pet.ticket.server.application.domain.model.User;
-import my.pet.ticket.server.application.domain.service.TokenService;
-import my.pet.ticket.server.application.domain.service.UserService;
 import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,39 +14,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class BearerAuthenticationReader implements GrpcAuthenticationReader {
 
-  private final TokenService tokenService;
-
-  private final UserService userService;
-
-  public BearerAuthenticationReader(TokenService tokenService, UserService userService) {
-    this.tokenService = tokenService;
-    this.userService = userService;
-  }
-
   @Nullable
   @Override
   public Authentication readAuthentication(ServerCall<?, ?> call, Metadata headers)
       throws AuthenticationException {
     String tokenFromHeader = headers.get(Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER));
     if (tokenFromHeader != null) {
-      Token token = this.tokenService.getTokenByToken(tokenFromHeader);
-      User user = this.userService.get(token.getUserId());
-      BearerAuthenticationToken bearerAuthenticationToken = new BearerAuthenticationToken(
-          token.getUserId(), token.getToken(), List.of(new GrantedAuthorityImpl(user.getRole())));
-      bearerAuthenticationToken.setAuthenticated(true);
-      return bearerAuthenticationToken;
-    }
-    return new AbstractAuthenticationToken(List.of()) {
-      @Override
-      public Object getCredentials() {
-        return null;
-      }
+      return new AbstractAuthenticationToken(List.of()) {
+        @Override
+        public Object getCredentials() {
+          return "anonymous";
+        }
 
-      @Override
-      public Object getPrincipal() {
-        return null;
-      }
-    };
+        @Override
+        public Object getPrincipal() {
+          return tokenFromHeader;
+        }
+      };
+    }
+    return null;
   }
 
 }
